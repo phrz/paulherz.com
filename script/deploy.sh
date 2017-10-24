68be4ba9be7bfaeb04f083cf924d27b44ad1c602
phrz <phrz@me.com>
2017-10-24 11:28:52 -0500

    nevermind travis

---
+++ script/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+  cd _site
+  git init
+
+  git config user.name "phrz"
+  git config user.email "phrz@me.com"
+
+  git add .
+  git commit -m "Deploy"
+
+  # We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+else
+  echo 'Invalid branch. You can only deploy from master.'
+  exit 1
+fi
